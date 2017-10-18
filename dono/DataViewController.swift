32370ad7242d03ab1fea00b4adfceb89d85a96f6
antkan <antkan@astutesolutions.com>
2017-10-18 15:47:55 -0400

    Initial Commit

---
+++ dono/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  dono
+//
+//  Created by Anthony Kantor on 10/18/17.
+//  Copyright © 2017 Anthony Kantor. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
